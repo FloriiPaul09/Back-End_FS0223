f7915c6c25724fb8a2709c24df666faa7a036c12
FloriiPaul09 <floriipaul@gmail.com>
2023-06-28 16:58:36 +0200

    for exercise

--- m1/s1/g3/compito/Esercizio 4 (for)/src/App.java
+++ m1/s1/g3/compito/Esercizio 4 (for)/src/App.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        int number = 11;
+        Scanner num = new Scanner(System.in);
+        System.out.println("Inserisci un numero. . . .");
+
+
+        int number = num.nextInt();
+        
         for (int i = number; i >= 0; i--) {
             System.out.println(i);
         }
