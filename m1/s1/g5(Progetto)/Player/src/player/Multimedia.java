2a30b20ef593dfa22f62fde21ebb04b696fc02d3
FloriiPaul09 <floriipaul@gmail.com>
2023-07-02 09:30:59 +0200

    classi audio e video

--- m1/s1/g5(Progetto)/Player/src/player/Multimedia.java
+++ m1/s1/g5(Progetto)/Player/src/player/Multimedia.java
@@ -1,5 +1,148 @@
 package player;
 
-abstract class Multimedia {
-    
+abstract public class Multimedia {
+    private String titolo;
+    private int durata;
+
+    public Multimedia(int durata, String titolo) {
+        this.titolo= titolo;
+        this.durata= durata;
+    }
+
+    public String getTitolo(){
+        return titolo;
+    }
+
+    public int getDurata(){
+        return durata;
+    }
+
+    public void play(){
+        System.out.println("");
+    } 
+
 }
+
+class Audio extends Multimedia implements PlayerInterface{
+
+    private int volume;
+    
+    public Audio (int durata, String titolo, int volume) {
+        super(durata, titolo);
+        this.volume= volume; 
+    }
+
+    public int getVolume(){
+        return volume;
+    }
+
+    public void setVolume(int volume){
+        this.volume = volume;
+    }
+
+    @Override
+    public int abassaVolume(int valore){
+        volume -= valore;
+        if(volume<0){
+            volume = 0;
+        }
+        return volume;
+    }
+
+    @Override
+    public int alzaVolume(int valore){
+        volume += valore;
+        return volume;
+    }
+
+	@Override
+	public int abassaLuminosita(int valore) {
+		return 0;
+	}
+
+	@Override
+	public int alzaLuminosita(int valore) {
+		return 0;
+	}
+
+    @Override
+    public void play(){
+        System.out.println("Riproduzione audio " + getTitolo());
+
+        for(int i = 0; i < getDurata(); i++){
+            String esclamativo = "!".repeat(volume);
+            System.out.println(getTitolo() + " " + esclamativo);
+        }
+    }
+}
+
+class Video extends Multimedia implements PlayerInterface{
+    private int volume;
+    private int luminosita;
+
+    public Video(String titolo, int durata, int volume, int luminosita){
+        super(durata, titolo);
+        this.volume = volume;
+        this.luminosita = luminosita;
+    }
+
+    public int getVolume(){
+        return volume;
+    }
+
+    public void setVolume(int volume){
+        this.volume = volume;
+    }
+
+    public int getLuminosita(){
+        return luminosita;
+    }
+
+    public void setLuminosita(int luminosita){
+        this.luminosita = luminosita;
+    }
+
+    @Override
+    public int abassaVolume(int valore){
+        volume -= valore;
+        if(volume<0){
+            volume = 0;
+        }
+        return volume;
+    }
+
+    @Override
+    public int alzaVolume(int valore){
+        volume += valore;
+        return volume;
+    }
+
+	@Override
+	public int abassaLuminosita(int valore) {
+		luminosita -= valore;
+        if (luminosita < 0){
+            luminosita = 0;
+        }
+        return luminosita;
+	}
+
+	@Override
+	public int alzaLuminosita(int valore) {
+		luminosita += valore;
+        return luminosita;
+	}
+
+    public void play(){
+        System.out.println("Riproduzione audio " + getTitolo());
+
+        for(int i = 0; i < getDurata(); i++){
+            String esclamativo = "!".repeat(volume);
+            String asterischi = "*".repeat(luminosita);
+            System.out.println(getTitolo() + " " + esclamativo + " " + asterischi );
+        }
+    }
+}
+
+
+
+
--- m1/s1/g5(Progetto)/Player/src/player/PlayerInterface.java
+++ m1/s1/g5(Progetto)/Player/src/player/PlayerInterface.java
@@ -2,4 +2,9 @@ package player;
 
 public interface PlayerInterface {
     
+    int abassaVolume(int valore);
+    int alzaVolume(int valore);
+
+    int abassaLuminosita(int valore);
+    int alzaLuminosita(int valore);
 }
