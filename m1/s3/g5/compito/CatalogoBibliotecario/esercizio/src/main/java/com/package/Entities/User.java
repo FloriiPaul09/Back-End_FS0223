78455fa77ecf3a7464f569d6623c7c2e98fd2f71
FloriiPaul09 <floriipaul@gmail.com>
2023-07-14 14:31:46 +0200

    User constructor&methods

--- m1/s3/g5/compito/CatalogoBibliotecario/esercizio/src/main/java/com/package/Entities/User.java
+++ m1/s3/g5/compito/CatalogoBibliotecario/esercizio/src/main/java/com/package/Entities/User.java
@@ -22,4 +22,43 @@ public class User {
     @Column(name = "card_number", unique = true)
     private Integer cardNumber;
 
+    //Constructor
+    public User() {}
+
+    //Name methods
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    //Surname methods
+    public String getSurname(){
+        return surname;
+    }
+
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+
+    //Birth date methods
+    public LocalDate getBirthDate(){
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate){
+        this.birthDate = birthDate;
+    }
+
+    //Card number methods
+    public Integer getCardNumber(){
+        return cardNumber;
+    }
+
+    public void setCardNumber(Integer cardNumber){
+        this.cardNumber = cardNumber;
+    }
+
 }
