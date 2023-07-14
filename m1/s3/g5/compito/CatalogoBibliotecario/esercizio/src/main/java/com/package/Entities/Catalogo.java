ca1b9b0caef522b51d4dd2fed516d7989aee6069
FloriiPaul09 <floriipaul@gmail.com>
2023-07-14 14:19:33 +0200

    Catalogo constructor&methods

--- m1/s3/g5/compito/CatalogoBibliotecario/esercizio/src/main/java/com/package/Entities/Catalogo.java
+++ m1/s3/g5/compito/CatalogoBibliotecario/esercizio/src/main/java/com/package/Entities/Catalogo.java
@@ -22,5 +22,53 @@ public class Catalogo {
     @Column (name = "num_pages")
     private Integer numPages;
 
+    //Constructor
+    public Catalogo() {}
+
+
+    //Id methods
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    //ISBN methods
+    public String getIsbn(){
+        return isbn;
+    }
+
+    public void setIsbn(String isbn){
+        this.isbn = isbn;
+    }
+
+    //Title methods
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    //Year of publication methods
+    public Integer getPublicationYear(){
+        return publicationYear;
+    }
+
+    public void setPublicationYear(){
+        this.publicationYear = publicationYear;
+    }
+
+    //Number of pages methods
+    public Integer getNumPages(){
+        return numPages;
+    }
+
+    public void setNumPages(){
+        this.numPages = numPages;
+    }
 
 }
